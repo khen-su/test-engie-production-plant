97751b28ef50878d003a9e6a25755e1bb6dbdf29
Victor vD <>
2020-05-23 16:01:02 +0200

    remove useless file and add websocket manager

---
+++ PowerPlantCodingChallenge/WebSocketManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Web.WebSockets;
+
+namespace PowerPlantCodingChallenge
+{
+    public class SimpleWebSocketHandler: WebSocketHandler
+
+    {
+
+        private static WebSocketCollection _wsClients = new WebSocketCollection();
+        public override void OnOpen()
+
+        {
+            _wsClients.Add(this);
+            base.OnOpen();
+        }
+
+        public override void OnClose()
+
+        {
+            _wsClients.Remove(this);
+            base.OnClose();
+        }
+
+        public void SendMessage(WebSocketResponse response)
+        {
+            OnOpen();
+            _wsClients.Broadcast(JsonSerializer.Serialize(response));
+        }
+        }
+    }
