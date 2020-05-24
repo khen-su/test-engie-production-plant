f5e12c9f1ab194e84c18a5d788471a6d023807d0
Victor vD <>
2020-05-24 13:59:55 +0200

    Add signalRClient program (previously excluded from solution)

---
+++ SimpleSignalRClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleSignalRClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a key to start listening..");
+            Console.ReadKey();
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:8888/ws")
+                .AddMessagePackProtocol()
+                .Build();
+
+            connection.On<string>("message", (message) =>
+                Console.WriteLine($"Message:\n{message}\n"));
+
+            connection.StartAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Listening. Press a key to quit");
+            Console.ReadKey();
+        }
+    }
+}
